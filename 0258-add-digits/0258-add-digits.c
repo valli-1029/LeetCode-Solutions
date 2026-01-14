9c3554d6f14b75d7f934579ce98ab68f019d4cca
pravalikapilla <pravalikapilla14@gmail.com>
2026-01-14 21:18:17 +0530

    Time: 0 ms (100%), Space: 8.4 MB (64.95%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.c
@@ -0,0 +1,13 @@
+int addDigits(int num) {
+    int sum;
+
+    while(num>=10){
+        sum=0;
+     while(num>0){
+        sum=sum+(num%10);
+        num=num/10;
+    }
+    num=sum;
+}
+return sum;
+}
