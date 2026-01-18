ea5a134db9935671c423ba1c2dc47a2acfa1d67c
pravalikapilla <pravalikapilla14@gmail.com>
2026-01-18 20:51:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (35.33%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,18 @@
+int mySqrt(int x) {
+    if (x==0 || x==1) return x;
+    int low=1, high=x, ans=0;
+
+    while (low<=high) {
+        long long mid=low+(-low+high)/2;
+
+        if (mid*mid==x)
+            return mid;
+        else if (mid*mid<x) {
+            ans=mid;
+            low=mid+1;
+        } else {
+            high=mid-1;
+        }
+    }
+    return ans;
+}
