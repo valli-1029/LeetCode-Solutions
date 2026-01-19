647e7b7068e39b3d11e885cf17bc2185ec92fe0e
pravalikapilla <pravalikapilla14@gmail.com>
2026-01-19 17:50:38 +0530

    Time: 0 ms (100%), Space: 7.7 MB (50.02%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.c
@@ -0,0 +1,16 @@
+bool isHappy(int n) {
+    long long sum=0,rem;
+    if(n==1) return true;
+    while(n!=1 && n!=4){
+        sum=0;
+        while(n>0){
+            
+            rem=n%10;
+            sum=sum+(rem*rem);
+            n=n/10;
+        }
+n=sum;
+    }
+    if(sum==1) return true;
+return false;
+}
