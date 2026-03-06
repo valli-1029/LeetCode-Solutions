e79ad7ae74fa12fa1253a248854736a56846c2d5
pravalikapilla <pravalikapilla14@gmail.com>
2026-03-06 21:37:41 +0530

    Time: 0 ms (100%), Space: 9.2 MB (6.42%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.c
+++ 0069-sqrtx/0069-sqrtx.c
@@ -1,18 +1,22 @@
 int mySqrt(int x) {
-    if (x==0 || x==1) return x;
-    int low=1, high=x, ans=0;
+    if (x==1|| x==0) return x;
+    int low=1, high=x, res=0;
+    long long mid;
 
-    while (low<=high) {
-        long long mid=low+(-low+high)/2;
-
-        if (mid*mid==x)
+    while(low<=high){
+        mid = low+(high-low)/2;
+        if(mid*mid==x){
             return mid;
-        else if (mid*mid<x) {
-            ans=mid;
+        }
+        else if(mid*mid<x){
+            res=mid;
             low=mid+1;
-        } else {
+        }
+        else {
             high=mid-1;
         }
+
     }
-    return ans;
+    return res;
+
 }
