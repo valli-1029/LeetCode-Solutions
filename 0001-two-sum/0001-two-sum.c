0d09ae9fcb022a389393c21fbefaf2ced31ecf32
pravalikapilla <pravalikapilla14@gmail.com>
2026-04-05 21:53:07 +0530

    Time: 99 ms (80.01%), Space: 9.3 MB (44.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -2,15 +2,15 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int *res=(int *)malloc(2* sizeof(int));
+    int *r=(int *)malloc(2* sizeof(int));
     *returnSize=2;
     for(int i=0;i<numsSize;i++){
         for(int j=i+1;j<numsSize;j++)
         if((nums[i]+nums[j])==target){
-            res[0]=i;
-            res[1]=j;
-            return res;
+            r[0]=i;
+            r[1]=j;
+            return r;
         }
     }
-    return res;
+    return r;
 }
